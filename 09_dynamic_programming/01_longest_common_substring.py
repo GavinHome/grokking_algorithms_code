ce7cd61a35d8ea56a273eeb862955d63b3470250
GavinHome <yangxiaoming512@yeah.net>
2025-08-26 17:01:24 +0800

    [feat] longest common substring

---
+++ 09_dynamic_programming/01_longest_common_substring.py
@@ -0,0 +1,16 @@
+dp_table_blue = ["b", "l", "u", "e"]
+dp_table_clues = ["c", "l", "u", "e", "s"]
+
+dp_table = [[0 for j in range(len(dp_table_clues))] for i in range(len(dp_table_blue))]
+
+for i in range(len(dp_table_blue)):
+    for j in range(len(dp_table_clues)):
+        if dp_table_clues[j] == dp_table_blue[i]:
+            dp_table[i][j] = dp_table[i-1][j-1] + 1
+        else:
+            dp_table[i][j] = 0
+
+
+# display table
+for i in dp_table:
+    print(i)
